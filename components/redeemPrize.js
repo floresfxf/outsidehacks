a5a1393149ac9a7b6f4c408c6ce0a0f3b8817f6d
Nikhil Anand <nikhil@fudinc.com>
2017-07-29 21:59:17 -0700

    claim prize

--- components/redeemPrize.js
+++ components/redeemPrize.js
@@ -15,6 +15,7 @@ import {
 } from 'react-native';
 import RPrizesAppBar from './redeemPrizeAppBar';
 import { Card, ListItem, Button } from 'react-native-elements';
+import Modal from 'react-native-modal';
 
 let WINDOW_WIDTH = Dimensions.get('window').width;
 let WINDOW_HEIGHT = Dimensions.get('window').height;
@@ -22,7 +23,13 @@ let WINDOW_HEIGHT = Dimensions.get('window').height;
 class Redeem extends React.Component {
   constructor(){
     super();
+    this.state = { isModalVisible: false };
   }
+
+  _showModal = () => this.setState({ isModalVisible: true })
+
+  _hideModal = () => this.setState({ isModalVisible: false })
+
   render() {
     return (
       <View style={styles.container}>
@@ -37,11 +44,22 @@ class Redeem extends React.Component {
               COPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPYCOPY
             </Text>
             <Button
+              onPress={this._showModal}
               backgroundColor='#03A9F4'
               buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='VIEW NOW' />
+              title='REDEEM' />
             </Card>
         </View>
+        <Modal isVisible={this.state.isModalVisible} backdropColor={'white'} backdropOpacity={0.75} >
+          <View>
+            <View style={{justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+            <Image source={require('../images/code.png')} />
+            </View>
+            <TouchableOpacity onPress={() => {this._hideModal()}}>
+              <Text style={{marginTop: 10, fontSize: 30, textAlign: 'center'}}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
       </View>
     )
   }
